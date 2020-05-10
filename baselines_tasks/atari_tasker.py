b53445143fbec2962ca8fee673e0927f2fecb560
Quint <DrKwint@gmail.com>
2020-05-10 17:04:02 -0500

    cleanup and bugfix

--- baselines_tasks/atari_tasker.py
+++ baselines_tasks/atari_tasker.py
@@ -7,14 +7,15 @@ import random
 
 from baselines_tasks.train_task import TrainTask, EvalTask
 
+env_type = ['atari']
+reward_shaping = [0, -0.001, -0.0025, -0.005, -0.01]
+augmentation = ['', 'constraint_state', 'action_history']
+
 def twod_tasks():
-    env_type = ['atari']
     env = ['Seaquest']
     constraint = ['2d_actuation4', '2d_actuation4_dense', '2d_actuation4_hard', '2d_dithering4', '2d_dithering4_dense', '2d_dithering4_hard']
-    reward_shaping = [0, -0.0025, -0.005, -0.01]
-    augmentation = ['', 'constraint_state', 'constraint_state_noembed', 'action_history']
-    train_seed = [7842, 1206, 8610]
-    eval_seed = [5722, 2579, 1892, 7583, 9238]
+    train_seed = []#7842, 1206, 8610]
+    eval_seed = []#5722, 2579, 1892, 7583, 9238]
     arg_names = [
         'env_type', 'env', 'constraint', 'reward_shaping', 'augmentation',
         'train_seed', 'eval_seed'
@@ -23,17 +24,16 @@ def twod_tasks():
         dict(zip(arg_names, arg_vals)) for arg_vals in itertools.product(
             env_type, env, constraint, reward_shaping, augmentation, train_seed, eval_seed)
     ]
+    args = [d for d in args if not ('hard' in d['constraint'] and d['reward_shaping'] != 0)]
     return [EvalTask(**a) for a in args]
+
 
 def oned_tasks():
     # per-task info
-    env_type = ['atari']
     env = ['Breakout', 'SpaceInvaders']
     constraint = ['1d_dithering2', '1d_dithering2_dense', '1d_dithering2_hard', '1d_actuation4', '1d_actuation4_dense', '1d_actuation4_hard']
-    reward_shaping = [0, -0.0025, -0.005, -0.01]
-    augmentation = ['', 'constraint_state', 'constraint_state_noembed', 'action_history']
-    train_seed = [7842, 1206, 8610]
-    eval_seed = [5722, 2579, 1892, 7583, 9238]
+    train_seed = []#7842, 1206, 8610]
+    eval_seed = []#5722, 2579, 1892, 7583, 9238]
     arg_names = [
         'env_type', 'env', 'constraint', 'reward_shaping', 'augmentation',
         'train_seed', 'eval_seed'
@@ -43,6 +43,7 @@ def oned_tasks():
         dict(zip(arg_names, arg_vals)) for arg_vals in itertools.product(
             env_type, env, constraint, reward_shaping, augmentation, train_seed, eval_seed)
     ]
+    args = [d for d in args if not ('hard' in d['constraint'] and d['reward_shaping'] != 0)]
     return [EvalTask(**a) for a in args]
 
 def breakout_tasks():
@@ -51,7 +52,7 @@ def breakout_tasks():
     env = ['Breakout']
     constraint = ['paddle_ball_distance', 'paddle_ball_distance_dense', 'paddle_ball_distance_hard']
     reward_shaping = [0, -0.0025, -0.005, -0.01]
-    augmentation = ['', 'constraint_state', 'constraint_state_noembed', 'action_history']
+    augmentation = ['', 'constraint_state', 'action_history']
     train_seed = [7842]#, 1206, 8610]
     eval_seed = [5722, 2579]#, 1892, 7583, 9238]
     arg_names = [
@@ -63,11 +64,11 @@ def breakout_tasks():
         dict(zip(arg_names, arg_vals)) for arg_vals in itertools.product(
             env_type, env, constraint, reward_shaping, augmentation, train_seed, eval_seed)
     ]
+    args = [d for d in args if not ('hard' in d['constraint'] and d['reward_shaping'] != 0)]
     return [EvalTask(**a) for a in args]
-    
-
 
 def create_tasks():
     tasks = oned_tasks() + twod_tasks() + breakout_tasks()
     random.shuffle(tasks)
     return tasks
+
